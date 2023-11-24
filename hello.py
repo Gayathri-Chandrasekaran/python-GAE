42b3b588b10ca173fe06123407569f2e89d20de6
Gayathri-Chandrasekaran <130374820+Gayathri-Chandrasekaran@users.noreply.github.com>
2023-11-24 20:09:12 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,26 +1,3 @@
-
-from flask import Flask
-
-app = Flask(__name__)
-
-def hello():
-    """Return a friendly HTTP greeting.
-
-    Returns:
-        A string with the words 'Hello World!'.
-    """
-    a=int(input("Enter num:"))
-    b=int(input("Enter num:"))
-    print(a+b)
-    return "Hello World!"
-
-@app.route("/")
-def route_hello():
-    return hello()
-
-if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding `entrypoint` to app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
-
+a=int(input("Enter num:"))
+b=int(input("Enter num:"))    
+print(a+b)
